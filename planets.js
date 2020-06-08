459fd97372d115fed75eebb47502abbb65d32f91
Juan José Londoño <juanjoselondonodavid@gmail.com>
2020-06-07 20:19:55 -0800

    additon of Titan

--- planets.js
+++ planets.js
@@ -7,17 +7,22 @@ var inputVal = document.getElementById("input_item");
 function mars (){
     var input = parseInt(inputVal.value)
     var mars_weigth = (input/9.81) * 3.711;
-    resulVal.innerHTML = `your weigth on mars is ${mars_weigth.toFixed(2)}`
+    resulVal.innerHTML = `your weigth on Mars is ${mars_weigth.toFixed(2)}`
 }
 function moon(){
     var input = parseInt(inputVal.value)
     var moon_weigth = (input/9.81) * 1.62;
-    resulVal.innerHTML = `your weigth on the moon is ${moon_weigth.toFixed(2)}`
+    resulVal.innerHTML = `your weigth on the Moon is ${moon_weigth.toFixed(2)}`
 }
 function jupiter (){
     var input = parseInt(inputVal.value)
     var jupiter_weigth = (input/9.81) * 24.79;
     resulVal.innerHTML = `your weigth on Jupiter is ${jupiter_weigth.toFixed(2)}` 
+}
+function titan (){
+    var input = parseInt(inputVal.value)
+    var jupiter_weigth = (input/9.81) * 1.352;
+    resulVal.innerHTML = `your weigth on Titan is ${jupiter_weigth.toFixed(2)}` 
 }
 
 
