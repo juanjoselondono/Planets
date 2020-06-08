40b3fb427ae3461d3d7226846da79f8ba59a8b29
Juan José Londoño <juanjoselondonodavid@gmail.com>
2020-06-07 20:37:48 -0800

    V - gamma

--- planets.js
+++ planets.js
@@ -6,23 +6,23 @@ var inputVal = document.getElementById("input_item");
 //Functions
 function mars (){
     var input = parseInt(inputVal.value)
-    var mars_weigth = (input/9.81) * 3.711;
-    resulVal.innerHTML = `your weigth on Mars is ${mars_weigth.toFixed(2)}`
+    var mars_weight = (input/9.81) * 3.711;
+    resulVal.innerHTML = `your weight on Mars is ${mars_weight.toFixed(2)}`
 }
 function moon(){
     var input = parseInt(inputVal.value)
-    var moon_weigth = (input/9.81) * 1.62;
-    resulVal.innerHTML = `your weigth on the Moon is ${moon_weigth.toFixed(2)}`
+    var moon_weight = (input/9.81) * 1.62;
+    resulVal.innerHTML = `your weight on the Moon is ${moon_weight.toFixed(2)}`
 }
 function jupiter (){
     var input = parseInt(inputVal.value)
-    var jupiter_weigth = (input/9.81) * 24.79;
-    resulVal.innerHTML = `your weigth on Jupiter is ${jupiter_weigth.toFixed(2)}` 
+    var jupiter_weight = (input/9.81) * 24.79;
+    resulVal.innerHTML = `your weight on Jupiter is ${jupiter_weight.toFixed(2)}` 
 }
 function titan (){
     var input = parseInt(inputVal.value)
-    var jupiter_weigth = (input/9.81) * 1.352;
-    resulVal.innerHTML = `your weigth on Titan is ${jupiter_weigth.toFixed(2)}` 
+    var jupiter_weight = (input/9.81) * 1.352;
+    resulVal.innerHTML = `your weight on Titan is ${jupiter_weight.toFixed(2)}` 
 }
 
 
