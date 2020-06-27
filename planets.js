4c4899365e7c3ec7b6a35c556102531e74b138fe
Juan José Londoño David <juanjoselondonodavid@gmail.com>
2020-06-27 11:29:34 -0800

    Bug fixed

--- planets.js
+++ planets.js
@@ -7,22 +7,22 @@ var inputVal = document.getElementById("input_item");
 function mars (){
     var input = parseInt(inputVal.value)
     var mars_weight = (input/9.81) * 3.711;
-    resulVal.innerHTML = `your weight on Mars is ${mars_weight.toFixed(2)}`
+    resulVal.innerHTML = `your weight on Mars is ${mars_weight.toFixed(2)} kg`
 }
 function moon(){
     var input = parseInt(inputVal.value)
     var moon_weight = (input/9.81) * 1.62;
-    resulVal.innerHTML = `your weight on the Moon is ${moon_weight.toFixed(2)}`
+    resulVal.innerHTML = `your weight on the Moon is ${moon_weight.toFixed(2)} kg`
 }
 function jupiter (){
     var input = parseInt(inputVal.value)
     var jupiter_weight = (input/9.81) * 24.79;
-    resulVal.innerHTML = `your weight on Jupiter is ${jupiter_weight.toFixed(2)}` 
+    resulVal.innerHTML = `your weight on Jupiter is ${jupiter_weight.toFixed(2)} kg` 
 }
 function titan (){
     var input = parseInt(inputVal.value)
     var jupiter_weight = (input/9.81) * 1.352;
-    resulVal.innerHTML = `your weight on Titan is ${jupiter_weight.toFixed(2)}` 
+    resulVal.innerHTML = `your weight on Titan is ${jupiter_weight.toFixed(2)} kg` 
 }
 
 
